19f061be2f97175c746f340a9c65a6e098dace69
samair <tsameerc@gmail.com>
2019-05-25 21:53:48 +0530

    push to heroku

--- src/main/java/com/webvidhi/stock/account/controller/AccountController.java
+++ src/main/java/com/webvidhi/stock/account/controller/AccountController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webvidhi.stock.account.model.Account;
@@ -17,6 +18,7 @@ import com.webvidhi.stock.account.service.AccountService;
 import io.swagger.annotations.Api;
 
 @RestController
+@RequestMapping("/acc/")
 @Api(value="Account Controller")
 public class AccountController {
 	
