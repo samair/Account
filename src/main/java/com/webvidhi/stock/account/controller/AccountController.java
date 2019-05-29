4d2dc315248d3dfad234c155637ab9a89e744c73
samair <tsameerc@gmail.com>
2019-05-29 09:40:21 +0530

    enable cross origin

--- src/main/java/com/webvidhi/stock/account/controller/AccountController.java
+++ src/main/java/com/webvidhi/stock/account/controller/AccountController.java
@@ -2,7 +2,9 @@ package com.webvidhi.stock.account.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +18,7 @@ import com.webvidhi.stock.account.model.User;
 import com.webvidhi.stock.account.service.AccountService;
 
 import io.swagger.annotations.Api;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/acc/")
 @Api(value="Account Controller")
